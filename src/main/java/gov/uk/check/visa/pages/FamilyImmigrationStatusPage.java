39c1815132511ecc15d51575c3e80e8a7030159f
pjethalia <pjethalia2005@gmail.com>
2023-12-19 20:47:35 +0000

    "Project"

---
+++ src/main/java/gov/uk/check/visa/pages/FamilyImmigrationStatusPage.java
@@ -0,0 +1,18 @@
+package gov.uk.check.visa.pages;
+
+import gov.uk.check.visa.utility.Utility;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+public class FamilyImmigrationStatusPage extends Utility {
+
+    private static final Logger log = LogManager.getLogger(FamilyImmigrationStatusPage.class.getName());
+
+    //nextStepButton, yes, no locators
+
+
+    //create methods 'void selectImmigrationStatus(String status)'
+    // (Note: Use switch statement for select immigration status) and 'void clickNextStepButton()'
+
+
+}
